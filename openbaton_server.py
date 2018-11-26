2564b5117b0babafb23d63448d289fc659545c1e
Allen Wang <37149217+allenw84@users.noreply.github.com>
2018-11-26 18:12:46 +0800

    Update openbaton_server.py

--- openbaton_server.py
+++ openbaton_server.py
@@ -4,14 +4,10 @@ import sys
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-# Set a timeout so the socket does not block
-# indefinitely when trying to receive data.
-#server.settimeout(0.2)
 HOST=''
 PORT = 44444
 server.bind((HOST, PORT))
-#server.listen()
-#conn, addr = s.accept() #receive TCP the connections
+
 dict={}
 IPlist=[]
 serviceName=''
@@ -24,7 +20,7 @@ while True:
 		serviceName += data
 		if serviceName not in dict:
 			dict[serviceName]=list() 
-	#dict[serviceName] = IPlist
+	
 	'''
 	for i in dict.items():
 		print(i[0],i[1])
